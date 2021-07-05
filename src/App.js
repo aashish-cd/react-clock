42d1a81aaf991fa8d9b9471071687c6eb44aea14
Aashish <bhattaashish303@gmail.com>
2021-07-05 20:41:37 +0545

    why not

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+ const [hours, setHours] = useState('')
+ const [minutes, setMinutes] = useState('')
+ const [seconds, setSeconds] = useState('')
+ const [meridian, setMeridain] = useState('AM !')
+
+ if (hours > 12) {
+  setMeridain("PM !")
+  setHours(hours - 12)
+ }
+
+ const setClockTime = () => {
+  var time = new Date();
+
+  setHours(time.getHours())
+  setMinutes(time.getMinutes())
+  setSeconds(time.getSeconds())
+
+
+ }
+ useEffect(() => {
+  setClockTime()
+ }, [seconds])
+
+ setTimeout(() => {
+  var time = new Date()
+  setSeconds(time.getSeconds());
+ }, 1000)
+
+ return <div className='container'>
+  <div className='header'>
+   <h1>current time</h1>
+
+  </div>
+  <div className='time'>
+
+   <h1>{hours < 10 ? `0${hours}` : hours}:
+    {minutes < 10 ? `0${minutes}` : minutes}:
+    {seconds < 10 ? `0${seconds}` : seconds}{' '}
+    {meridian}
+   </h1>
+
+  </div>
+ </div>
 }
 
-export default App;
+export default App
